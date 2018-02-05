9358411ebe93d3935983a17e2ddd85dbca5b22fa
kvench <32459535+kvench@users.noreply.github.com>
2018-02-05 18:12:53 +0300

    пули

--- танки.py
+++ танки.py
@@ -1,3 +1,4 @@
+from random import choice
 from tkinter import Tk, Canvas, mainloop, NW
 from PIL import Image, ImageTk
 
@@ -48,7 +49,10 @@ player_image_up = ImageTk.PhotoImage((Image.open("tank.gif").resize((block_width
 player_image_right = ImageTk.PhotoImage((Image.open("tank.gif").resize((block_width, block_height))).rotate(270))
 player_image_down = ImageTk.PhotoImage((Image.open("tank.gif").resize((block_width, block_height))).rotate(180))
 player_image_left = ImageTk.PhotoImage((Image.open("tank.gif").resize((block_width, block_height))).rotate(90))
-
+bullet_image_up = ImageTk.PhotoImage((Image.open("bullet.png").convert('RGBA').resize((block_width, block_height))).rotate(90))
+bullet_image_right = ImageTk.PhotoImage((Image.open("bullet.png").convert('RGBA').resize((block_width, block_height))))
+bullet_image_down = ImageTk.PhotoImage((Image.open("bullet.png").convert('RGBA').resize((block_width, block_height))).rotate(270))
+bullet_image_left = ImageTk.PhotoImage((Image.open("bullet.png").convert('RGBA').resize((block_width, block_height))).rotate(180))
 # координаты игрока
 x = 6
 y = 6
@@ -58,11 +62,17 @@ player_right = c.create_image(x * block_width, y * block_height, image= player_i
 player_down = c.create_image(x * block_width, y * block_height,  image= player_image_down, anchor=NW)
 player_left = c.create_image(x * block_width, y * block_height,  image= player_image_left, anchor=NW)
 
-c.itemconfigure(player_left, state='normal')
+
+c.itemconfigure(player_left, state='hidden')
 c.itemconfigure(player_up, state='hidden')
 c.itemconfigure(player_down, state='hidden')
 c.itemconfigure(player_right, state='hidden')
+c.itemconfigure(choice([player_up, player_down, player_left, player_right]), state='normal')
 
+
+
+
+bullets = []
 # проверка доступности клетки
 def is_available(i, j):
     if i < 0 or i >= 12 or j < 0 or j >= 12:
@@ -71,14 +81,20 @@ def is_available(i, j):
         return False
     return True
 
+def moveBullets():
+    for bullet in bullets:
+        c.move(bullet, 10, 0)
+    c.after(moveBullets)
+
 # нажатие клавиши
 def keyDown(key):
-    global x, y, player_up, player_down, player_left, player_right
+    global x, y, player_up, player_down, player_left, player_right, bullet_up, bullet_down, bullet_right, bullet_left
     if key.char == 'a':
         c.itemconfigure(player_left, state='normal')
         c.itemconfigure(player_up, state='hidden')
         c.itemconfigure(player_down, state='hidden')
         c.itemconfigure(player_right, state='hidden')
+
         if is_available(x - 1, y):
             x -= 1
 
@@ -87,6 +103,7 @@ def keyDown(key):
         c.itemconfigure(player_up, state='hidden')
         c.itemconfigure(player_down, state='hidden')
         c.itemconfigure(player_right, state='normal')
+
         if is_available(x + 1, y):
             x += 1
     if key.char == 'w':
@@ -94,6 +111,7 @@ def keyDown(key):
         c.itemconfigure(player_up, state='normal')
         c.itemconfigure(player_down, state='hidden')
         c.itemconfigure(player_right, state='hidden')
+
         if is_available(x, y - 1):
             y -= 1
     if key.char == 's':
@@ -101,8 +119,16 @@ def keyDown(key):
         c.itemconfigure(player_up, state='hidden')
         c.itemconfigure(player_down, state='normal')
         c.itemconfigure(player_right, state='hidden')
+
+
         if is_available(x, y + 1):
             y += 1
+    if key.char==' ':
+        bullets.append(c.create_image(x * block_width, y * block_height,  image= bullet_image_left, anchor=NW))
+        
+
+
+
     c.coords(player_up, x * block_width, y * block_height)
     c.coords(player_down, x * block_width, y * block_height)
     c.coords(player_left, x * block_width, y * block_height)
@@ -115,5 +141,5 @@ def keyDown(key):
 #при нажатии любой клавишы вызываем keyDown
 
 tk.bind("<KeyPress>", keyDown)
-
+c.after(50, moveBullets)
 mainloop()
